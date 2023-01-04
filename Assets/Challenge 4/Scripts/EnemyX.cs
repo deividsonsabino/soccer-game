e72b26af36a9f2784ad228a4391b910efd8e8c38
Deividson Sabino <xdeividsabinox@live.com>
2023-01-03 23:03:04 -0300

    add: enemy follow the goal

--- Assets/Challenge 4/Scripts/EnemyX.cs
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 100f;
     private Rigidbody enemyRb;
     private GameObject playerGoal;
 
@@ -12,6 +12,7 @@ public class EnemyX : MonoBehaviour
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
     }
 
     // Update is called once per frame
